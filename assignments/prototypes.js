2187671f1e2fd835904b723bd26325d77de3c092
sean-attewell <sean.attewell@gmail.com>
2019-01-23 16:10:16 +0000

    test of classes is working, pre-made not working

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,5 +1,5 @@
 /*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
+  Object oriented design is commonly used in video games. For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
 
   In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
 
@@ -7,13 +7,27 @@
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
-  
+console.log("*****************New game*****************")
 /*
   === GameObject ===
   * createdAt
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
+
+function GameObject(CreatedAt, dimensions) {
+  this.CreatedAt = CreatedAt;
+  this.dimensions = dimensions;
+  // this.name = "frankerz"
+}
+
+
+GameObject.prototype.destroy = function () {
+  return `${this.name} was removed from the game.`;
+}
+
+// const GOtest = new GameObject({length: 1, width: 2, height: 3});
+// GOtest.destroy();
 
 /*
   === CharacterStats ===
@@ -22,6 +36,21 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(CreatedAt,dimensions, healthPoints, name) {
+  GameObject.call(this, CreatedAt, dimensions);    // call parent's constructor, fixing `this` to that of the child instance being created
+  this.healthPoints = healthPoints;
+  this.name = name;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);  //  here we set up the inheritance
+
+CharacterStats.prototype.takeDamage = function () {
+  return `${this.name} took damage`;
+}
+
+// stattest = new CharacterStats({length: 1, width: 2, height: 3}, 15, 'Henry')
+
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -33,6 +62,22 @@
   * should inherit takeDamage() from CharacterStats
 */
  
+function Humanoid(CreatedAt, dimensions, healthPoints, name, team, weapons, language) {
+  CharacterStats.call(this, CreatedAt, dimensions, healthPoints, name);    // call parent's constructor, fixing `this` to that of the child instance being created
+  this.team = team;
+  this.weapons = weapons;
+  this.language = language;
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);  //  here we set up the inheritance
+
+Humanoid.prototype.greet = function () {
+  return `${this.name} offers a greeting in ${this.language}.`;
+}
+
+const humanoidTest = new Humanoid(new Date(), {length: 1, width: 2, height: 3}, 15, 'Henry', 'Reading FC', ['mace'], 'welsh');
+
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,68 +86,68 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
 
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  healthPoints: 5,
+  name: 'Bruce',
+  team: 'Mage Guild',
+  weapons: [
+    'Staff of Shamalama',
+  ],
+  language: 'Common Tongue',
+});
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  healthPoints: 15,
+  name: 'Sir Mustachio',
+  team: 'The Round Table',
+  weapons: [
+    'Giant Sword',
+    'Shield',
+  ],
+  language: 'Common Tongue',
+});
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  healthPoints: 10,
+  name: 'Lilith',
+  team: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'Elvish',
+});
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,84 @@
-/* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+/* The four principles of "this";
+* in your own words. explain the four principles for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding
+* 'this' points at the window/console object when in the global scope.
 *
+* 2. Implicit Binding
+* Whenever a function is called by a preceding dot, the object before that dot is 'this'.
+*
+* 3. New Binding
+* Whenever a constructor function is used, this refers to the specific instance of the object that is created and returned by the constructor function.
+*
+* 4. Explicit binding
+* Whenever JavaScript’s call or apply method is used, this is explicitly defined.
+
+
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
 // code example for Window Binding
 
-// Principle 2
+console.log(this);   // in the global scope
 
+function sayName(name) {    // function defined in the gloabl scope
+    console.log(this);
+    return name;
+  }
+
+ console.log(sayName("Oliver"));
+
+// Principle 2
 // code example for Implicit Binding
 
-// Principle 3
+const myObj = {
+    greeting: 'Hello',
+    sayHello: function(name) {
+      console.log(this);
+      console.log(`${this.greeting} my name is ${name}`);
+    }
+  };
 
+ myObj.sayHello('Ryan');
+ 
+  
+//  const sayNameFunc = obj => {
+//     obj.sayName = function() {
+//       console.log(`Hello my name is ${this.name}`);
+//       console.log(this);
+//     };
+//   };
+  
+// const me = { name: 'Ryan' };
+// const you = { name: 'Freddy' };
+// sayNameFunc(me);
+// sayNameFunc(you);
+
+
+ 
+// Principle 3
 // code example for New Binding
 
-// Principle 4
+function CordialPerson(greeter) {
+    this.greeting = 'Hello ';
+    this.greeter = greeter;
+    this.speak = function() {
+      console.log(this.greeting + this.greeter);
+      console.log(this);
+    };
+  }
+  
+  const jerry = new CordialPerson('Newman');
+  const newman = new CordialPerson('Jerry');
+  
+  jerry.speak();
+//   newman.speak();
 
+
+// Principle 4
 // code example for Explicit Binding
+
+jerry.speak.call(newman);
+// newman.speak.apply(jerry);
+
